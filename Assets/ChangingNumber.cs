76b3f3e5aa1176c7f34c6ad9d6f2247edef305e4
Chi-Sheng Yang <chisheng@usc.edu>
2021-02-05 22:57:46 +0800

    Currency System and home base

---
+++ Assets/ChangingNumber.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangingNumber : MonoBehaviour
+{
+    public Text currency;
+    private float animationTime = 1.5f;
+    private float currNumber, goalNumber, initial;
+  
+    public void AddToNumber(int value)
+    {
+        initial = currNumber;
+        goalNumber += value;
+    }
+
+    public void SubToNumber(int value)
+    {
+        initial = currNumber;
+        goalNumber -= value;
+    }
+
+    public void SetToZero()
+    {
+        initial = currNumber;
+        goalNumber = 0;
+    }
+
+    public void Update()
+    {
+        if(currNumber != goalNumber)
+        {
+            float diff;
+            if(initial < goalNumber)
+            {
+                diff = goalNumber - initial;
+                currNumber += (animationTime * Time.deltaTime) * diff;
+                if (currNumber >= goalNumber)
+                    currNumber = goalNumber;
+            }
+            else
+            {
+                diff = initial - goalNumber;
+                currNumber -= (animationTime * Time.deltaTime) * diff;
+                if (currNumber <= goalNumber)
+                    currNumber = goalNumber;
+            }
+            currency.text = currNumber.ToString("0");
+        }
+    }
+}
---
+++ Assets/playerEvent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerEvent : MonoBehaviour
+{
+    public GameObject panel;
+    public GameObject mask;
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (panel != null)
+        {
+            panel.SetActive(true);
+            mask.SetActive(false);
+        }
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (panel != null)
+        {
+            panel.SetActive(false);
+            mask.SetActive(true);
+        }
+    }
+}
