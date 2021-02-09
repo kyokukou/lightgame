d5d5bb6c63ccc22499ffec1e1a056545ea6ecd14
zhanghaomeng <haomengz@usc.edu>
2021-02-09 11:44:08 -0800

    destroy demo

---
+++ Assets/Scripts/DestroyLight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyLight : MonoBehaviour
+{
+    public GameObject trigger;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+    	if (other.tag == "Player") {
+    		Destroy(trigger);
+    	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
